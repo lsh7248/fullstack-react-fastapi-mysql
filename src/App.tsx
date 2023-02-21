609b8e75750eaae41fc1707fd70759a545bd50a6
LeeSeongHyun <leesh7248@gmail.com>
2023-02-21 14:31:43 +0900

    material ui "Blog" Template Test

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,15 @@
 import { useState } from 'react';
 import reactLogo from './assets/react.svg';
 import './App.css';
+import Blog from './components/blog/Blog';
 
 function App() {
   const [count, setCount] = useState(0);
 
   return (
     <div className="App">
-      <div>
+      <Blog />
+      {/* <div>
         <a href="https://vitejs.dev" target="_blank">
           <img src="/vite.svg" className="logo" alt="Vite logo" />
         </a>
@@ -26,7 +28,7 @@ function App() {
       </div>
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
-      </p>
+      </p> */}
     </div>
   );
 }
